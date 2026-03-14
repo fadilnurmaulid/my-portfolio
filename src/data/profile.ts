56267b032d732390401bccd9690501c72170a85d
fadil-nurmaulid <fadilnurmaulid01@gmail.com>
2026-03-14 15:39:09 +0700

    Fix username

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -8,9 +8,9 @@ location: "Depok, Indonesia",
 
 email: "fadilnurmaulid01@gmail.com",
 
-github: "https://github.com/fadil-nurmaulid",
+github: "https://github.com/fadilnurmaulid",
 
-linkedin: "https://linkedin.com/in/fadil-nurmaulid",
+linkedin: "https://linkedin.com/in/fadilnurmaulid",
 
 about:
 "Physics Instrumentation undergraduate at University of Indonesia with a strong focus on artificial intelligence, computational science, electronics, embedded systems, and IoT. I am driven to integrate analytical thinking, programming expertise, and scientific methods to develop intelligent systems and innovative technological solutions. My interests span the intersection of physical instrumentation and computational technologies, aiming to advance research and practical applications in AI-driven and data-intensive environments."
